e8473435de78449b31c64519b2e41bb1729239af
Federico Reina <federicoreinavinchira@yahoo.com>
2020-11-28 17:06:47 -0500

    elegir genero pt. 1

---
+++ Assets/Scripts/BotonGenero.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BotonGenero : MonoBehaviour
+{
+    Transform transform;
+    Text txtGenero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform =  GetComponent<Transform>();
+        txtGenero = transform.GetChild(0).gameObject.GetComponent<Text>();
+    }
+
+    public void PonerTexto(string texto){
+        txtGenero.text = texto;
+    }
+}
---
+++ Assets/Scripts/GenreManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenreManager : MonoBehaviour
+{
+    static List<string> generos = new List<string>{
+        "Homenaje",
+        "Zelda",
+        "RPG",
+        "Aventura",
+        "Deportes",
+        "Accion",
+        "Comida",
+        "Musical",
+        "Animales",
+        "Jammer"
+    };
+
+    List<string> generosSeleccionados;
+
+    public List<GameObject> objGeneros;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        generosSeleccionados = SeleccionarNAleatorios(objGeneros.Count);
+        updateText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void updateText(){
+        for (int i = 0; i < generosSeleccionados.Count; ++i){
+            string genero = generosSeleccionados[i];
+            var objeto = objGeneros[i];
+            objeto.GetComponent<BotonGenero>().PonerTexto(genero);
+        }
+    }
+
+    List<string> SeleccionarNAleatorios(int n){
+        int sz = generos.Count;
+        int restantes = n;
+        List<string> lista2 = new List<string>();
+        for(int i = 0; i < sz && restantes > 0; ++i){
+            if (Random.Range(i,sz-1) >= sz-restantes){
+                lista2.Add(generos[i]);
+                --restantes;
+            }
+        }
+        return lista2;
+    }
+}
