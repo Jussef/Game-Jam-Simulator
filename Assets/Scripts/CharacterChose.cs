f8e0af24b77138992c98c4f2d263d4d8eb1fe143
kurei <tamaotamamura@gmail.com>
2020-11-27 22:57:25 -0600

    se modifico la escena para encontrar a los miembros de un equipo

--- Assets/Scripts/CharacterChose.cs
+++ Assets/Scripts/CharacterChose.cs
@@ -6,6 +6,13 @@ using UnityEngine.UI;
 
 public class CharacterChose : MonoBehaviour, IPointerClickHandler
 {
+    public GameObject contador;
+    public GameObject panel;
+    [SerializeField] private List<GameObject> imagenes;
+    [SerializeField] private List<GameObject> elegidos;
+
+    private List<string> miembros = new List<string>() { "Programador(a)", "Diseñador(a)", "Musico(a)" };
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,5 +29,22 @@ public class CharacterChose : MonoBehaviour, IPointerClickHandler
     {
         bool outline = gameObject.GetComponentInChildren<Outline>().enabled;
         gameObject.GetComponentInChildren<Outline>().enabled = !outline;
+        int iContador = int.Parse(contador.GetComponent<Text>().text);
+        iContador++;
+        if (iContador < 2)
+            contador.GetComponent<Text>().text = iContador + "";
+        else {
+            panel.SetActive(true);
+            int i = 0;
+            foreach (GameObject imagen in imagenes)
+            {
+                if (imagen.GetComponentInChildren<Outline>().enabled)
+                {
+                    elegidos[i].GetComponent<Image>().sprite = imagen.GetComponent<Image>().sprite;
+                    elegidos[i].transform.Find("Text").GetComponent<Text>().text = miembros[Random.Range(0,2)];
+                    i++;
+                }
+            }
+        }
     }
 }
