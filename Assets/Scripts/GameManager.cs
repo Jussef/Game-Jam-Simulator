bae3346e7cb9da7ec667433a480d27664f32053a
Federico Reina <federicoreinavinchira@yahoo.com>
2020-11-28 02:46:07 -0500

    mostrar puntajes y mejorado el flujo

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,24 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public Text respuestaText;
     public Text puntajeText;
+    public List<Text> puntajesEnemigosTexts;
+
+
     List<Pregunta> preguntas;
     int preguntaActual;
     bool finalizado;
     bool habilitarClick;
-    float puntajeTotal;
+    public float puntajeTotal;
+    List<float> puntajesEnemigos;
+    const int NUM_ENEMIGOS = 3;
+    public int puestoFinal;
+
+    private static System.Random rand = new System.Random();
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+        
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
+
+        puntajesEnemigos = new List<float>();
+        for (int i = 0; i < NUM_ENEMIGOS; ++i){
+            puntajesEnemigos.Add(0f);
+        }
+
+        // * Para agregar una pregunta, debe tener exactamente 6 guiones bajos en cada puesto.
         Pregunta[] prs = {
-            new Pregunta("El personaje tendrá que correr a través de ______ y esquivar ______", 2),
+            new Pregunta("El personaje tendra que correr a traves de ______ y esquivar ______", 2),
             new Pregunta("El jugador puede ahorrar para mejorar su ______", 1)
         };
         preguntas = new List<Pregunta>(prs);
@@ -28,14 +50,17 @@ public class GameManager : MonoBehaviour
         ActualizarText();
         habilitarClick = true;
         puntajeTotal = 0;
+        puestoFinal = 0;
     }
 
     public void RecibirRespuesta(Respuesta respuesta){
         habilitarClick = false;
         puntajeTotal += respuesta.puntaje;
+        for (int i = 0; i < NUM_ENEMIGOS; ++i){
+            puntajesEnemigos[i] += Random.Range(10,25);
+        }
         preguntas[preguntaActual].AgregarRespuesta(respuesta);
         if (preguntas[preguntaActual].EstaCompleta()){
-            
             if (preguntaActual == preguntas.Count - 1){
                 FinDelJuego();
             }else{
@@ -49,6 +74,9 @@ public class GameManager : MonoBehaviour
     void ActualizarText(){
         respuestaText.text = preguntas[preguntaActual].pregunta;
         puntajeText.text = $"Puntos: {puntajeTotal}";
+        for (int i = 0; i < NUM_ENEMIGOS; ++i){
+            puntajesEnemigosTexts[i].text = $"Puntaje: {puntajesEnemigos[i]}";
+        }
     }
 
     Pregunta DarPreguntaActual(){
@@ -58,15 +86,23 @@ public class GameManager : MonoBehaviour
     void FinDelJuego(){
         finalizado = true;
         print("Fin del juego");
+        CalcularPuesto();
+        SceneManager.LoadScene("FinDeJuego");
+    }
+
+    void CalcularPuesto(){
+        puestoFinal = 1;
+        for(int i = 0; i < NUM_ENEMIGOS; ++i){
+            if (puntajeTotal < puntajesEnemigos[i]) ++puestoFinal;
+        }
     }
 
     public bool SePuedeOprimirBoton(){
         return habilitarClick && !finalizado;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public bool HaGanado(){
+        return puestoFinal == 1;
     }
+
 }
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+
+    public Text textEstatusFinJuego,
+                textNombreGameJam,
+                textTema,
+                textMainPage,
+                textSubPage,
+                textBottomPage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        if (GameManager.instance){
+            GameManager gameManager = GameManager.instance;
+            bool gano = gameManager.HaGanado();
+            string estatus = gano ? "¡Has Ganado!" : "Perdiste :C";
+            textEstatusFinJuego.text = $"{estatus}\nHas quedado en el puesto {gameManager.puestoFinal}";
+            textMainPage.text = $"Ronda 1 - Obtuviste {gameManager.puntajeTotal} puntos";
+            //textSubPage.text = $"lol";
+            textBottomPage.text = gano ? "¡Felicidades ganando en esta gamejam!" : "¡Mejor suerte para la próxima!";
+            Destroy(gameManager.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
